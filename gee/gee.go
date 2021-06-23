804cecd64d5d3ab60a9567d71d1845c1cacf3aff
justniu <1573996821@qq.com>
2021-06-23 14:26:06 +0800

    context wrap w and req

--- gee/gee.go
+++ gee/gee.go
@@ -1,23 +1,21 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 ) 
 
-type HandlerFunc func(w http.ResponseWriter, req *http.Request)
+type HandlerFunc func(c *Context)
 
 type Engine struct{
-	router map[string]HandlerFunc 
+	router *router
 }
 
 func New() *Engine{
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc){
-	key := method + "-" + pattern
-	e.router[key] = handler 
+	e.router.addRoute(method, pattern, handler)
 }
 
 func (e *Engine) GET(pattern string, handler HandlerFunc){
@@ -33,11 +31,7 @@ func (e *Engine) Run(addr string) (err error){
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request){
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := e.router[key]; ok{
-		handler(w, req)
-	}else{
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := newContext(w, req)
+	e.router.handle(c)
 }
 
